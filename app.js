779db64dc494f95182d2b118a48bfb742ccc41ed
Saurabh Anand <saurabhanand7777@gmail.com>
2020-03-21 10:21:06 +0530

    port change

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000 || process.env.PORT, process.env.IP, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
   console.log("Tales server has Started !");
 });
